8544f6184985ef3cc0b5a5f4184220ea760bcce3
Ionut <isustic@Ionuts-MacBook-Pro.local>
2026-05-03 17:12:09 +0300

    VPC, subnet, internet gateway

--- main.tf
+++ main.tf
@@ -8,5 +8,61 @@ terraform {
 }
 
 provider "aws" {
-  region = "euc-central-1"
+  region = "eu-central-1"
+}
+
+
+# Create a VPC
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "pixel-tracker-vpc"
+  }
+}
+
+# Public subnet
+
+resource "aws_subnet" "public" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "eu-central-1a"
+
+  tags = {
+    Name = "pixel-tracker-public-subnet"
+  }
+}
+
+# Internet gateway
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "pixel-tracker-igw"
+  }
+}
+
+
+# Route table to point all traffic to IGW
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "pixel-tracking-public-rt"
+  }
+}
+
+# Associate route table with subnet
+
+resource "aws_route_table_association" "public" {
+  subnet_id = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
 }
